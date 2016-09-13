f528e93f8537c2a05307dca40a5b29f47f09ae10
Robin Winslow <robin@robinwinslow.co.uk>
2016-09-13 15:46:08 +0100

    Make a proper package
    Add setup.py to make ubuntudesign.documentation_builder a proper package.
    
    Also provide a command-line script in `bin/documentation-builder`.

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='ubuntudesign.documentation-builder',
+    version='0.0.1',
+    author='Canonical webteam',
+    author_email='robin+pypi@canonical.com',
+    url='https://github.com/ubuntudesign/documentation-builder',
+    packages=[
+        'ubuntudesign.documentation_builder',
+    ],
+    description=(
+        'A command-line tool for building documentation from repositories '
+        'into HTML files. Initially based on '
+        'https://github.com/juju/docs/blob/master/tools/mdbuild.py.'
+    ),
+    scripts=['bin/documentation-builder'],
+    # long_description=open('README.md').read(),
+    install_requires=["Markdown>=2.6.0"],
+)
---
+++ ubuntudesign/documentation_builder/cli.py
@@ -0,0 +1,5 @@
+def main():
+    raise NotImplementedError("The CLI isn't ready yet.")
+
+if __name__ == "__main__":
+    main()
