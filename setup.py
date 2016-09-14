91d6f19a256ce7b745ac38d4de03d84b002dab60
Robin Winslow <robin@robinwinslow.co.uk>
2016-09-14 09:59:24 +0100

    New point version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='ubuntudesign.documentation-builder',
-    version='0.0.1',
+    version='0.0.2',
     author='Canonical webteam',
     author_email='robin+pypi@canonical.com',
     url='https://github.com/ubuntudesign/documentation-builder',
