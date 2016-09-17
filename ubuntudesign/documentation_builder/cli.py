c55b91c35d5ba2c8cdd6ee53979813d3aaf874e0
Robin Winslow <robin@robinwinslow.co.uk>
2016-09-17 11:52:47 +0100

    Add the option for a preprocessor

--- ubuntudesign/documentation_builder/cli.py
+++ ubuntudesign/documentation_builder/cli.py
@@ -1,7 +1,9 @@
 # Core modules
 import argparse
+import sys
+from glob import glob
+from os import getcwd, path
 from tempfile import TemporaryDirectory
-from os.path import join
 
 # Third party modules
 from git import Repo
@@ -42,8 +44,26 @@ def parse_arguments():
         '--nav-path',
         help="Path to a local nav file."
     )
+    parser.add_argument(
+        '--preprocessor',
+        help="A python function for preprocessing the markdown files."
+    )
 
     return parser.parse_args()
+
+
+def preprocess_files(dir_path, preprocessor_string):
+    """
+    Given a directory path and a string representing a python function,
+    run each file in the directory through the function.
+    """
+
+    (package, function) = preprocessor_string.split(':')
+    sys.path.append(getcwd())
+    preprocessor = getattr(__import__(package), function)
+
+    for filename in glob(dir_path + '/**/*.md', recursive=True):
+        preprocessor(filename)
 
 
 def main():
@@ -57,8 +77,13 @@ def main():
     with TemporaryDirectory(prefix='/dev/shm/') as temp_source_folder:
         Repo.clone_from(arguments.source_repository, temp_source_folder)
 
+        if arguments.preprocessor:
+            preprocess_files(temp_source_folder, arguments.preprocessor)
         build(
-            source=join(temp_source_folder, arguments.files_path.strip('/')),
+            source=path.join(
+                temp_source_folder,
+                arguments.files_path.strip('/')
+            ),
             outpath=arguments.destination_folder,
             template_path=arguments.template_path,
             nav_path=arguments.nav_path
