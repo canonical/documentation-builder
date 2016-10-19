548a0135f43c126423a55dd467fa5d4271a8828f
Robin Winslow <robin@robinwinslow.co.uk>
2016-10-19 18:29:16 +0200

    Version 0.4.2

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ assert sys.version_info >= (3, 5), (
 
 setup(
     name='ubuntudesign.documentation-builder',
-    version='0.4.1',
+    version='0.4.2',
     author='Canonical webteam',
     author_email='robin+pypi@canonical.com',
     url='https://github.com/ubuntudesign/documentation-builder',
