5c01ce2a882d4ff0beefa98c0bc382df596d5563
Robin Winslow <robin@robinwinslow.co.uk>
2017-07-21 12:12:28 +0200

    Update to 1.5.0

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ assert sys.version_info >= (3, 5), (
 
 setup(
     name='ubuntudesign.documentation-builder',
-    version='1.4.3',
+    version='1.5.0',
     author='Canonical webteam',
     author_email='robin+pypi@canonical.com',
     url='https://github.com/ubuntudesign/documentation-builder',
